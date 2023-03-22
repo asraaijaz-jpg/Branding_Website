763a3967b83714a1941db90da6e447b4194a8ecd
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-22 12:56:05 +0500

    comiit

--- branding-website/src/App.js
+++ branding-website/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header'
+import Header from './Components/Header';
+import {BrowserRouter , Route , Link , Routes} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+
+      </Routes>
+      </BrowserRouter>
       <Header/>
     </div>
   );
