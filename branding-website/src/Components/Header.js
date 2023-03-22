9006bf21a4188557b84368af8d3f41a326026aee
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-22 12:49:28 +0500

    commit

---
+++ branding-website/src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Style.css';
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+        <a href='Home'>Home</a>
+            <a href='About'>About</a>
+            <a href='Contact'>Contact</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
