d9cff53547fd84580e2957e79074a1a63dfd72ab
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-25 01:41:10 +0500

    commit

--- branding-website/src/App.js
+++ branding-website/src/App.js
@@ -20,7 +20,6 @@ function App() {
     <Header/>
 
     <Routes>
-
         <Route path='/' element={<Home/>}/>
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
